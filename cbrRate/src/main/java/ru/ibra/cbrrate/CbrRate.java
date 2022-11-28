8541b9f4ad8d86749739aa11455e5165b80d7e47
Ibra <safdas213@yandex.ru>
2022-11-28 23:28:28 +0300

    init commit

--- cbrRate/src/main/java/ru/ibra/cbrrate/CbrRate.java
+++ cbrRate/src/main/java/ru/ibra/cbrrate/CbrRate.java
@@ -5,7 +5,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CbrRate {
-
     public static void main(String[] args) {
         new SpringApplicationBuilder().sources(CbrRate.class).run(args);
     }
